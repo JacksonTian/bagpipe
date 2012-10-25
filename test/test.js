be293fd05891a9781b39253c8c0a68ac91aa1b73
Jackson Tian <shyvo1987@gmail.com>
2012-10-25 12:40:36 +0800

    Update.

--- test/test.js
+++ test/test.js
@@ -11,10 +11,28 @@ describe('bagpipe', function () {
     bagpipe.limit.should.be.equal(10);
     bagpipe.queue.should.have.length(0);
     bagpipe.active.should.be.equal(0);
+    bagpipe.disabled.should.be.equal(false);
+  });
+
+  it('constructor disabled', function () {
+    var bagpipe = new Bagpipe(10, true);
+    bagpipe.limit.should.be.equal(10);
+    bagpipe.queue.should.have.length(0);
+    bagpipe.active.should.be.equal(0);
+    bagpipe.disabled.should.be.equal(true);
   });
 
   it('constructor limit less than 1', function (done) {
     var bagpipe = new Bagpipe(0);
+    bagpipe.push(async, [100], function () {
+      bagpipe.active.should.be.equal(0);
+      done();
+    });
+    bagpipe.active.should.be.equal(0);
+  });
+
+  it('constructor disabled is true', function (done) {
+    var bagpipe = new Bagpipe(10, true);
     bagpipe.push(async, [100], function () {
       bagpipe.active.should.be.equal(0);
       done();
@@ -28,6 +46,27 @@ describe('bagpipe', function () {
     bagpipe.queue.should.have.length(0);
     bagpipe.active.should.be.equal(0);
     bagpipe.push(async, [100], function () {
+      bagpipe.active.should.be.equal(0);
+      done();
+    });
+    bagpipe.active.should.be.equal(1);
+  });
+
+  it('push, async with this', function (done) {
+    var bagpipe = new Bagpipe(5);
+    bagpipe.limit.should.be.equal(5);
+    bagpipe.queue.should.have.length(0);
+    bagpipe.active.should.be.equal(0);
+    var context = {value: 10};
+    context.async = function (callback) {
+      this.value--;
+      var that = this;
+      process.nextTick(function() {
+        callback(that.value);
+      });
+    };
+
+    bagpipe.push(context.async, [], function () {
       bagpipe.active.should.be.equal(0);
       done();
     });
@@ -50,6 +89,25 @@ describe('bagpipe', function () {
         }
       });
       bagpipe.active.should.not.be.above(limit);
+    }
+  });
+
+  it('push, disabled, active should not be above limit', function (done) {
+    var limit = 5;
+    var bagpipe = new Bagpipe(limit, true);
+    bagpipe.limit.should.be.equal(limit);
+    bagpipe.queue.should.have.length(0);
+    bagpipe.active.should.be.equal(0);
+    var counter = 10;
+    for (var i = 0; i < counter; i++) {
+      bagpipe.push(async, [100 + Math.round(Math.random() * 10)], function () {
+        bagpipe.active.should.be.equal(0);
+        counter--;
+        if (counter === 0) {
+          done();
+        }
+      });
+      bagpipe.active.should.be.equal(0);
     }
   });
 });
